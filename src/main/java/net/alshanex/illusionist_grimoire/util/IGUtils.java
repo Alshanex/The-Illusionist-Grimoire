bd84a6d1410b5a75bc0cbdb2a5be75e0f3a2604e
Alshanex <discordpermacc02@gmail.com>
2025-10-03 12:50:25 +0200

    Tweaked illusion resisting mechanic

--- src/main/java/net/alshanex/illusionist_grimoire/util/IGUtils.java
+++ src/main/java/net/alshanex/illusionist_grimoire/util/IGUtils.java
@@ -5,10 +5,13 @@ import net.minecraft.world.entity.LivingEntity;
 
 public class IGUtils {
     public static boolean canMobBypassIllusions(LivingEntity entity) {
-        return entity.getAttributeValue(AttributeRegistry.EVOCATION_MAGIC_RESIST) > 1.2 || entity.getType().is(ModTags.ILLUSION_IMMUNE_ENTITIES);
+        boolean resistEvocation = entity.getAttributeValue(AttributeRegistry.EVOCATION_MAGIC_RESIST) > 1.2;
+        boolean resistNormal = entity.getAttributeValue(AttributeRegistry.SPELL_RESIST) > 1.2;
+        return resistEvocation || resistNormal || entity.getType().is(ModTags.ILLUSION_IMMUNE_ENTITIES);
     }
 
     public static boolean canBypassIllusions(LivingEntity entity, double illusionistSpellPower) {
-        return entity.getAttributeValue(AttributeRegistry.EVOCATION_MAGIC_RESIST) > 1 + (Math.max(0, (illusionistSpellPower - 1)) / 1.5);
+        double resist = (1 - entity.getAttributeValue(AttributeRegistry.EVOCATION_MAGIC_RESIST)) + ((1 - entity.getAttributeValue(AttributeRegistry.SPELL_RESIST) * 0.5));
+        return resist > (Math.max(0, (illusionistSpellPower - 1)) / 1.5);
     }
 }
