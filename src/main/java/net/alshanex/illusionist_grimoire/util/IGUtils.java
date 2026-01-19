af146ef32c5982b30a8eeb11b53e4a3fed0f47c2
Alshanex <discordpermacc02@gmail.com>
2026-01-19 17:18:00 +0100

    Adjusted illusion resist formula

--- src/main/java/net/alshanex/illusionist_grimoire/util/IGUtils.java
+++ src/main/java/net/alshanex/illusionist_grimoire/util/IGUtils.java
@@ -5,12 +5,12 @@ import net.minecraft.world.entity.LivingEntity;
 
 public class IGUtils {
     public static boolean canMobBypassIllusions(LivingEntity entity) {
-        boolean resistEvocation = entity.getAttributeValue(AttributeRegistry.EVOCATION_MAGIC_RESIST) > 1.2;
+        boolean resistEvocation = entity.getAttributeValue(AttributeRegistry.SPELL_RESIST) > 1.2;
         return resistEvocation || entity.getType().is(ModTags.ILLUSION_IMMUNE_ENTITIES);
     }
 
     public static boolean canBypassIllusions(LivingEntity entity, double illusionistSpellPower) {
-        double resist = (1 - entity.getAttributeValue(AttributeRegistry.EVOCATION_MAGIC_RESIST));
+        double resist = (1 - entity.getAttributeValue(AttributeRegistry.SPELL_RESIST));
         return resist > (Math.max(0, (illusionistSpellPower - 1)) / 1.5);
     }
 }
